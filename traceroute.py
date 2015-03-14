0a4c16a35ac48e3b11dfa84e7b02e1a19f487e2a
AtnesNess <atnes@bk.ru>
2015-03-14 21:45:13 +0500

    --help, -d added

--- traceroute.py
+++ traceroute.py
@@ -6,6 +6,7 @@ import sys
 import socket
 import re
 import whois
+import argparse
 
 
 def isIP(adr):
@@ -15,12 +16,12 @@ def isIP(adr):
     return False
 
 
-def main():
+def main(args):
 
     tracert_port = 33434
     dests = []
     max_hops = 30
-    for arg in sys.argv[1:]:
+    for arg in args.destinations:
         dests.append(arg)
     icmp = socket.getprotobyname('icmp')
     udp = socket.getprotobyname('udp')
@@ -30,9 +31,12 @@ def main():
         if isIP(addr):
             dest = addr
         else:
-            dest = socket.gethostbyname(addr)
+            try:
+                dest = socket.gethostbyname(addr)
+            except Exception:
+                print("Unknown destination: {}".format(addr))
+                break
         ttl = 1
-        last_reached = True
         while True:
             recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
             send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
@@ -45,10 +49,11 @@ def main():
             curr_addr = None
             try:
                 curr_addr = recv_socket.recvfrom(512)[1][0]
-                try:
-                    curr_name = socket.gethostbyaddr(curr_addr)[0]
-                except socket.error:
-                    curr_name = curr_addr
+                if not args.dontresolve:
+                    try:
+                        curr_name = socket.gethostbyaddr(curr_addr)[0]
+                    except socket.error:
+                        curr_name = curr_addr
             except socket.error:
                 pass
             finally:
@@ -56,35 +61,36 @@ def main():
                 recv_socket.close()
 
             if curr_addr is not None:
-                try:
-                    provider = whois.get_whois_dict(curr_addr, "whois.iana.org")
-                    while provider == {}:
+                if not args.dontresolve:
+                    try:
                         provider = whois.get_whois_dict(curr_addr, "whois.iana.org")
-                    whois_data = whois.get_whois_dict(curr_addr, provider["refer"])
-                    while whois_data == {}:
+                        while provider == {}:
+                            provider = whois.get_whois_dict(curr_addr, "whois.iana.org")
                         whois_data = whois.get_whois_dict(curr_addr, provider["refer"])
-                    if "origin" in whois_data:
-                        origin = whois_data["origin"]
-                    else:
-                        origin = "Unknown"
-                    if "country" in whois_data:
-                        country = whois_data["country"]
-                    else:
-                        country = "Unknown"
-                    whois_info = "asn: {} counrty: {}".format(origin, country)
-                except KeyError:
-                    whois_info = "LOCAL"
+                        while whois_data == {}:
+                            whois_data = whois.get_whois_dict(curr_addr, provider["refer"])
+                        if "origin" in whois_data:
+                            origin = whois_data["origin"]
+                        else:
+                            origin = "Unknown"
+                        if "country" in whois_data:
+                            country = whois_data["country"]
+                        else:
+                            country = "Unknown"
+                        whois_info = "asn: {} country: {}".format(origin, country)
+                    except KeyError:
+                        whois_info = "LOCAL"
 
-                curr_host = "{} ({}) {}".format(curr_name if dest != curr_addr else addr,
+                    curr_host = "{} ({}) {}".format(curr_name if dest != curr_addr else addr,
                                                 curr_addr, whois_info)
+                else:
+                    curr_host = "{}".format(curr_addr)
             else:
                 curr_host = "* * *"
-            if last_reached or curr_addr is not None:
-                print("{}\t{}".format(ttl, curr_host))
+
             if curr_addr is not None:
-                last_reached = True
-            else:
-                last_reached = False
+                print("{}\t{}".format(ttl, curr_host))
+
             if curr_addr == dest:
                 break
             if ttl >= max_hops:
@@ -93,4 +99,9 @@ def main():
             ttl += 1
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Trace the route to the destination.")
+    parser.add_argument("destinations", metavar='IPs', nargs='+', help="destination IPs")
+    parser.add_argument("-d", "--dontresolve",action="store_true",  help="Specifies to not resolve addresses"
+                                                    " to host names and define country with ASN.")
+    args = parser.parse_args()
+    main(args)
